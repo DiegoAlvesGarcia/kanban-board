90ee4ea491cb189c1c91f97e7f0e3814e86cd720
Diego Alves Garcia <diego.alvesgarcia@gmail.com>
2023-02-22 04:07:04 -0300

    feat: testes unitários do componente card

--- src/app/shared/components/card/card.component.spec.ts
+++ src/app/shared/components/card/card.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CardMock } from '../../mocks/card.mock';
 
 import { CardComponent } from './card.component';
 
@@ -8,9 +10,10 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [CardComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +24,26 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should to test edit function', () => {
+    jest.spyOn(component.editCard, 'emit');
+    component.edit(CardMock)
+    expect(component.editCard.emit).toHaveBeenCalledWith(CardMock)
+  });
+  it('should to test delete function', () => {
+    jest.spyOn(component.deleteCard, 'emit');
+    component.delete(CardMock)
+    expect(component.deleteCard.emit).toHaveBeenCalledWith(CardMock)
+  });
+  it('should to test next function', () => {
+    jest.spyOn(component.nextStep, 'emit');
+    component.next(CardMock)
+    expect(component.nextStep.emit).toHaveBeenCalledWith(CardMock)
+  });
+  it('should to test previous function', () => {
+    jest.spyOn(component.previousStep, 'emit');
+    component.previous(CardMock)
+    expect(component.previousStep.emit).toHaveBeenCalledWith(CardMock)
   });
 });
