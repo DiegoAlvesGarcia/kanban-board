255d634766ff53f7736e0915f8d503f1a08795ff
Diego Alves Garcia <diego.alvesgarcia@gmail.com>
2023-02-22 02:04:03 -0300

    feat: componentes e modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BoardComponent } from './pages/board/board.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogTaskComponent } from './shared/components/dialog-task/dialog-task.component';
+import { MatInputModule } from '@angular/material/input';
+import { CardComponent } from './shared/components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BoardComponent,
+    DialogTaskComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,7 +29,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     ReactiveFormsModule,
     DragDropModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
