939c6e42159854ba4e8a2e6fd49269c8a0351b51
Diego Alves Garcia <diego.alvesgarcia@gmail.com>
2023-02-22 13:30:21 -0300

    fix: its de chamadas de API do cards.service

--- src/app/shared/services/cards-service/cards.service.spec.ts
+++ src/app/shared/services/cards-service/cards.service.spec.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { fakeAsync, TestBed } from '@angular/core/testing';
+import { fakeAsync, flush, TestBed } from '@angular/core/testing';
+import { first } from 'rxjs';
 import { steps } from '../../enums/steps.enum';
 import { CardMock } from '../../mocks/card.mock';
 import { httpClientStub } from '../../stubs/http-cliente.stub';
-
 import { CardsService } from './cards.service';
 
 describe('CardsService', () => {
@@ -22,38 +22,23 @@ describe('CardsService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should be teste getCards function', fakeAsync(() => {
-    service.getCards().subscribe((resp) => {
-      expect(resp).toEqual({})
-    })
-  }))
+  it('should be teste getCards function', () => {
+    service.getCards();
+    expect(httpClientStub.get).toHaveBeenCalled()
+  })
 
   it('should be teste postCard function', fakeAsync(() => {
-    service.postCard(CardMock).subscribe(() => {
-      service.getCards().subscribe((resp) => {
-        expect(resp).toEqual(CardMock)
-      })
-    })
+    service.postCard(CardMock);
+    expect(httpClientStub.post).toHaveBeenCalled()
   }))
 
   it('should be teste putCard function', fakeAsync(() => {
-    service.postCard(CardMock).subscribe(() => {
-      const cardEdited = CardMock;
-      cardEdited.titulo = steps.SECOND_STEP
-      service.putCard(cardEdited).subscribe(() => {
-        service.getCards().subscribe((resp) => {
-          expect(resp).toEqual(cardEdited)
-        })
-      })
-    })
+    service.putCard(CardMock);
+    expect(httpClientStub.put).toHaveBeenCalled()
   }))
+
   it('should be teste deleteCard function', fakeAsync(() => {
-    service.postCard(CardMock).subscribe(() => {
-      service.deleteCard(CardMock).subscribe(() => {
-        service.getCards().subscribe((resp) => {
-          expect(resp).toEqual({})
-        })
-      })
-    })
+    service.deleteCard(CardMock);
+    expect(httpClientStub.delete).toHaveBeenCalled()
   }))
 });
